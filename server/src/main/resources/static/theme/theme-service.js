43f73edd1d49b78c52bdb070880ea3bb0bdad5af
MatIvan <mativ.rufous@yandex.ru>
2024-02-11 10:03:32 +0300

    dark theme style

--- server/src/main/resources/static/theme/theme-service.js
+++ server/src/main/resources/static/theme/theme-service.js
@@ -1,11 +1,15 @@
 
+/** @type {HTMLBodyElement} */
+var body;
+
 /** @type {HTMLInputElement} */
 var switchButton;
 
 function init() {
+	body = document.querySelector("body");
 	switchButton = document.querySelector("#theme-switcher-input");
 	switchButton.onclick = () => {
-		console.log(switchButton.checked);
+		body.classList.toggle("dark");
 	}
 }
 
